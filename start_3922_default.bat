71c9f6bea43deddec7dc26e96047f7feaefe3edf
saikoucl <schulzmusicchile@gmail.com>
2021-06-30 19:24:39 -0400

    inicio

---
+++ start_3922_default.bat
@@ -0,0 +1,3 @@
+@echo off
+C:/Users/Sound/Desktop/MAIPUCITY/server//FXServer.exe +set serverProfile "default"
+pause
---
+++ txData/PlumeESX_DC3EC1.base/ABRIR.bat
@@ -0,0 +1,2 @@
+cd /d "C:\Users\Sound\Desktop\MAIPUCITY\server-data"
+"C:\Users\Sound\Desktop\MAIPUCITY\server"\FXServer.exe +exec server.cfg
